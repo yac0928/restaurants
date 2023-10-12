1f5674b2cd0c002fc9acf5599b6348b11458c91b
yac0928 <kangarooinapark@gmail.com>
2023-10-12 16:21:19 +0800

    build: improve static files

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
+const restaurants = require('./public/jsons/restaurant.json').results
+
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect('restaurants')
+})
+
+app.get('/restaurants', (req, res) => {
+  res.render('index', {layout: 'main'})
+})
+
+app.listen(port, () => {
+  console.log(`express server is running on http://localhost:${port}`)
+})
